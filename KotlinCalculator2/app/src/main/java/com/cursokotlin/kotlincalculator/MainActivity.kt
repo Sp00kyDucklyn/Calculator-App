775e681fc53f39c388a11d3cec21892460a5fdd7
Sp00kyDucklyn <hoshi.saga.dokuringo@gmail.com>
2024-01-26 20:47:04 -0700

    comienzo

--- KotlinCalculator2/app/src/main/java/com/cursokotlin/kotlincalculator/MainActivity.kt
+++ KotlinCalculator2/app/src/main/java/com/cursokotlin/kotlincalculator/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.cursokotlin.kotlincalculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import android.graphics.Color
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -11,23 +14,190 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.cursokotlin.kotlincalculator.ui.theme.KotlinCalculatorTheme
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+    private var canAddOperation = false
+    private var canAddDecimal = true
+    private lateinit var workingTV:TextView
+    private lateinit var resultTV:TextView
+    private lateinit var root: View
+    private lateinit var random: Random
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            KotlinCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_main)
+        workingTV = findViewById(R.id.workingsTV)
+        resultTV = findViewById(R.id.resultsTV)
+        root = findViewById(R.id.root)
+    }
+
+    fun numberAction(view: View)
+    {
+        if(view is Button)
+        {
+            if(view.text == ".")
+            {
+                if(canAddDecimal)
+                    workingTV.append(view.text)
+
+                canAddDecimal = false
             }
+            else
+                workingTV.append(view.text)
+
+            canAddOperation = true
         }
     }
+
+    fun operationAction(view: View)
+    {
+        if(view is Button && canAddOperation)
+        {
+            workingTV.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
+        }
+    }
+
+    fun allClearAction(view: View)
+    {
+        workingTV.text = ""
+        resultTV.text = ""
+    }
+
+    fun backSpaceAction(view: View)
+    {
+        val length = workingTV.length()
+        if(length > 0)
+            workingTV.text = workingTV.text.subSequence(0, length - 1)
+    }
+
+    fun equalsAction(view: View)
+    {
+        resultTV.text = calculateResults()
+    }
+
+    private fun calculateResults(): String
+    {
+        val digitsOperators = digitsOperators()
+        if(digitsOperators.isEmpty()) return ""
+
+        val timesDivision = timesDivisionCalculate(digitsOperators)
+        if(timesDivision.isEmpty()) return ""
+
+        val result = addSubtractCalculate(timesDivision)
+        return result.toString()
+    }
+
+    private fun addSubtractCalculate(passedList: MutableList<Any>): Float
+    {
+        var result = passedList[0] as Float
+
+        for(i in passedList.indices)
+        {
+            if(passedList[i] is Char && i != passedList.lastIndex)
+            {
+                val operator = passedList[i]
+                val nextDigit = passedList[i + 1] as Float
+                if (operator == '+')
+                    result += nextDigit
+                if (operator == '-')
+                    result -= nextDigit
+            }
+        }
+
+        return result
+    }
+
+    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any>
+    {
+        var list = passedList
+        while (list.contains('x') || list.contains('/'))
+        {
+            list = calcTimesDiv(list)
+        }
+        return list
+    }
+
+    private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any>
+    {
+        val newList = mutableListOf<Any>()
+        var restartIndex = passedList.size
+
+        for(i in passedList.indices)
+        {
+            if(passedList[i] is Char && i != passedList.lastIndex && i < restartIndex)
+            {
+                val operator = passedList[i]
+                val prevDigit = passedList[i - 1] as Float
+                val nextDigit = passedList[i + 1] as Float
+                when(operator)
+                {
+                    'x' ->
+                    {
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '/' ->
+                    {
+                        newList.add(prevDigit / nextDigit)
+                        restartIndex = i + 1
+                    }
+                    else ->
+                    {
+                        newList.add(prevDigit)
+                        newList.add(operator)
+                    }
+                }
+            }
+
+            if(i > restartIndex)
+                newList.add(passedList[i])
+        }
+
+        return newList
+    }
+
+    private fun digitsOperators(): MutableList<Any>
+    {
+        val list = mutableListOf<Any>()
+        var currentDigit = ""
+        for(character in workingTV.text)
+        {
+            if(character.isDigit() || character == '.')
+                currentDigit += character
+            else
+            {
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(character)
+            }
+        }
+
+        if(currentDigit != "")
+            list.add(currentDigit.toFloat())
+
+        return list
+    }
+
+
+    private fun randomColor(view: View){
+        val red = (0..255).random()
+        val green = (0..255).random()
+        val blue = (0..255).random()
+
+        val color = Color.rgb(red, green, blue)
+
+        view.setBackgroundColor(color)
+    }
+
+
+
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
